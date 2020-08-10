ec9a2b42c1efb255ac3cd7c2e686a42ccf1dbe53
u-masao <4973920+u-masao@users.noreply.github.com>
2020-08-10 18:07:05 +0900

    multipage

--- src/questions.py
+++ src/questions.py
@@ -1,6 +1,6 @@
-import click
-from src.util import output_pdf
 import random
+import click
+from src.util import QuestionSheet
 
 
 def question_diff(max_number=10):
@@ -27,12 +27,12 @@ def question_ans(max_number=10, width=0):
     return questions, theme
 
 
-def question_add_one():
+def question_add_one(max_number=10):
     theme = "いちたすなにか"
     questions = []
     op = "+"
     for _ in range(20):
-        question = "{}{}{}=".format(1, op, random.randint(0, 9))
+        question = "{}{}{}=".format(1, op, random.randint(0, max_number))
         questions.append(question)
     return questions, theme
 
@@ -41,37 +41,23 @@ def question_add_one():
 @click.argument("output_dir", type=click.Path(exists=True), default="reports")
 def main(output_dir):
 
-    for count in range(1, 11):
-        questions, theme = question_add_one()
-        output_pdf(
-            "{}/{}_{:02d}.pdf".format(output_dir, theme, count),
-            questions,
-            theme,
-        )
+    generators = [
+        {"func": question_add_one, "kwargs": {}},
+        {"func": question_diff, "kwargs": {}},
+        {"func": question_ans, "kwargs": {"max_number": 5, "width": 1}},
+        {"func": question_ans, "kwargs": {"max_number": 10, "width": 1}},
+    ]
 
-    for count in range(1, 11):
-        questions, theme = question_diff(max_number=10)
-        output_pdf(
-            "{}/{}_{:02d}.pdf".format(output_dir, theme, count),
-            questions,
-            theme,
-        )
-
-    for count in range(1, 11):
-        questions, theme = question_ans(max_number=5, width=1)
-        output_pdf(
-            "{}/{}_{:02d}.pdf".format(output_dir, theme, count),
-            questions,
-            theme,
-        )
-
-    for count in range(1, 11):
-        questions, theme = question_ans(max_number=10, width=1)
-        output_pdf(
-            "{}/{}_{:02d}.pdf".format(output_dir, theme, count),
-            questions,
-            theme,
-        )
+    for generator in generators:
+        sheets = None
+        for _ in range(1, 31):
+            questions, theme = generator["func"](**generator["kwargs"])
+            if sheets is None:
+                sheets = QuestionSheet("{}/{}.pdf".format(output_dir, theme))
+            sheets.draw(
+                questions=questions, theme=theme,
+            )
+        sheets.close()
 
 
 if __name__ == "__main__":
--- src/util.py
+++ src/util.py
@@ -5,72 +5,60 @@ from reportlab.platypus import Table
 from reportlab.lib.units import cm
 
 
-def open_pdf(output, author="", title="", subject=""):
-    pdf_canvas = canvas.Canvas(output)
-    pdf_canvas.saveState()
+class QuestionSheet:
+    def __init__(
+        self, output, author="author", title="title", subject="subject"
+    ):
+        self.pdf_canvas = canvas.Canvas(output)
+        self.pdf_canvas.setAuthor(author)
+        self.pdf_canvas.setTitle(title)
+        self.pdf_canvas.setSubject(subject)
+        self.pdf_canvas.setPageSize((21.0 * cm, 29.7 * cm))
 
-    pdf_canvas.setAuthor(author)
-    pdf_canvas.setTitle(title)
-    pdf_canvas.setSubject(subject)
+    def set_font(self, font_name, font_size):
+        pdfmetrics.registerFont(UnicodeCIDFont(font_name))
+        self.pdf_canvas.setFont(font_name, font_size)
 
-    # A4
-    pdf_canvas.setPageSize((21.0 * cm, 29.7 * cm))
+    def close(self):
+        self.pdf_canvas.save()
 
-    # B5
-    # pdf_canvas.setPageSize((18.2*cm, 25.7*cm))
+    def build_cells(self, questions):
+        cells = []
+        index = 0
+        for a in range(1, 11):
+            row = []
+            for b in range(1, 3):
+                cell = questions[index]
+                index = (1 + index) % len(questions)
+                row.append(cell)
+            cells.append(row)
+        return cells
 
-    return pdf_canvas
+    def draw(
+        self,
+        questions=["0+0="],
+        theme="なし",
+        page_title="ぱぱのさんすうもんだい",
+        font_name="HeiseiKakuGo-W5",
+    ):
 
+        cells = self.build_cells(questions)
 
-def set_font(pdf_canvas, font_name, font_size):
-    pdfmetrics.registerFont(UnicodeCIDFont(font_name))
-    pdf_canvas.setFont(font_name, font_size)
+        self.set_font(font_name, 30)
+        self.pdf_canvas.drawString(2 * cm, 26 * cm, page_title)
 
+        self.set_font(font_name, 20)
+        self.pdf_canvas.drawString(3 * cm, 25 * cm, "「=」のみぎがわに、ただしいすうじをかいてね")
 
-def close_pdf(pdf_canvas):
-    pdf_canvas.restoreState()
-    pdf_canvas.save()
+        self.set_font(font_name, 20)
+        self.pdf_canvas.drawString(5 * cm, 2 * cm, "かかったじかん＿＿＿＿ふん＿＿＿＿びょう")
 
+        self.set_font(font_name, 12)
+        self.pdf_canvas.drawString(5 * cm, 1 * cm, "テーマ「{}」".format(theme))
 
-def build_cells(questions):
-    cells = []
-    index = 0
-    for a in range(1, 11):
-        row = []
-        for b in range(1, 3):
-            cell = questions[index]
-            index = (1 + index) % len(questions)
-            row.append(cell)
-        cells.append(row)
-    return cells
+        table = Table(cells, colWidths=7 * cm, rowHeights=2 * cm)
+        table.setStyle([("FONT", (0, 0), (-1, -1), font_name, 28)])
+        w, h = table.wrapOn(self.pdf_canvas, 0, 0)
+        table.drawOn(self.pdf_canvas, 3 * cm, 24 * cm - h)
 
-
-def output_pdf(
-    output,
-    questions=["0+0="],
-    theme="なし",
-    page_title="ぱぱのさんすうもんだい",
-    font_name="HeiseiKakuGo-W5",
-):
-
-    cells = build_cells(questions)
-
-    pdf_canvas = open_pdf(output)
-    set_font(pdf_canvas, font_name, 30)
-    pdf_canvas.drawString(2 * cm, 26 * cm, page_title)
-
-    set_font(pdf_canvas, font_name, 20)
-    pdf_canvas.drawString(3 * cm, 25 * cm, "「=」のみぎがわに、ただしいすうじをかいてね")
-
-    set_font(pdf_canvas, font_name, 20)
-    pdf_canvas.drawString(5 * cm, 2 * cm, "かかったじかん＿＿＿＿ふん＿＿＿＿びょう")
-
-    set_font(pdf_canvas, font_name, 12)
-    pdf_canvas.drawString(5 * cm, 1 * cm, "テーマ「{}」".format(theme))
-
-    table = Table(cells, colWidths=7 * cm, rowHeights=2 * cm)
-    table.setStyle([("FONT", (0, 0), (-1, -1), font_name, 28)])
-    w, h = table.wrapOn(pdf_canvas, 0, 0)
-    table.drawOn(pdf_canvas, 3 * cm, 24 * cm - h)
-
-    close_pdf(pdf_canvas)
+        self.pdf_canvas.showPage()
