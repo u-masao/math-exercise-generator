e7fdcc2d89592078160076bae5ddae39057ca3db
u-masao <4973920+u-masao@users.noreply.github.com>
2020-08-16 15:32:31 +0900

    change admin site header. change admin polls interface.

--- math_execrcise/math_execrcise/settings.py
+++ math_execrcise/math_execrcise/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "math_execrcise.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- math_execrcise/polls/admin.py
+++ math_execrcise/polls/admin.py
@@ -1,5 +1,25 @@
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ["question_text"]
+    list_filter = ["pub_date"]
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    filedsets = [
+        (None, {"fields": ["question_text"]}),
+        (
+            "Date information",
+            {"fields": ["pub_date"], "classes": ["collapse"]},
+        ),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
