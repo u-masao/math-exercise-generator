a9708628927960ccec84bf7267fe54e6437c7ef7
u-masao <4973920+u-masao@users.noreply.github.com>
2020-08-16 09:28:56 +0900

    view から model へアクセスするサンプル

--- math_execrcise/polls/views.py
+++ math_execrcise/polls/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render  # noqa: F401
-
-# Create your views here.
 from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}.".format(question_id))
+
+
+def results(request, question_id):
+    response = "You're looking at the result of question {}."
+    return HttpResponse(response.format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}.".format(question_id))
