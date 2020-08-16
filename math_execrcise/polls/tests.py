a7e84a02358cbbfa45c1644a6cc2eb9900a65827
u-masao <4973920+u-masao@users.noreply.github.com>
2020-08-16 14:33:04 +0900

    add test. add query filter.

--- math_execrcise/polls/tests.py
+++ math_execrcise/polls/tests.py
@@ -1,9 +1,15 @@
 import datetime
 
+from django.urls import reverse
 from django.test import TestCase
 from django.utils import timezone
 
 from .models import Question
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionModelTests(TestCase):
@@ -21,3 +27,59 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+
+    def test_no_questions(self):
+        text = "Past question."
+        create_question(question_text=text, days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"],
+            ["<Question: {}>".format(text)],
+        )
+
+    def test_future_question(self):
+        text = "Future question."
+        create_question(question_text=text, days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        text = "Future question."
+        create_question(question_text=text, days=30)
+        text = "Past question."
+        create_question(question_text=text, days=-30)
+
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"],
+            ["<Question: {}>".format(text)],
+        )
+
+    def test_two_past_questions(self):
+        expect_list = []
+        for days in [-5, -30]:
+            text = "Past question {}.".format(days)
+            create_question(question_text=text, days=days)
+            expect_list.append("<Question: {}>".format(text))
+
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+            response.context["latest_question_list"], expect_list
+        )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(
+            question_text="Future question.", days=5
+        )
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text="Pastquestion.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
--- math_execrcise/polls/views.py
+++ math_execrcise/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from django.db.models import F
 
 from .models import Choice, Question
@@ -12,12 +13,17 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
