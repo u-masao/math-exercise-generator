4f09e89531a37066d689c072bd25b0f526d874ea
u-masao <4973920+u-masao@users.noreply.github.com>
2020-08-16 10:07:53 +0900

    add template

--- math_execrcise/polls/views.py
+++ math_execrcise/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render  # noqa: F401
+from django.shortcuts import get_object_or_404, render  # noqa: F401
 from django.http import HttpResponse
 
 from .models import Question
@@ -6,12 +6,13 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question {}.".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
