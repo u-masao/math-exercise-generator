5d307ed2953e178c8d8d01e51d78a329c81f555c
u-masao <4973920+u-masao@users.noreply.github.com>
2020-08-22 14:09:28 +0900

    問題生成クラスを作成しインタフェースを規定した

--- math_execrcise/sheets/urls.py
+++ math_execrcise/sheets/urls.py
@@ -6,21 +6,27 @@ app_name = "sheets"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path(
-        "add_specific_ab/<int:pages>/<int:ab_max>/<int:a>/<int:b>/",
-        views.add_specific_ab,
-        name="add_specific_ab",
+        (
+            "addition_specific_ab/<int:pages>/<int:ab_min>/"
+            "<int:ab_max>/<int:a>/<int:b>/"
+        ),
+        views.addition_specific_ab,
+        name="addition_specific_ab",
     ),
     path(
         (
-            "add_specific_ans/<int:pages>/<int:ab_max>/"
-            "<int:ans>/<int:width>/<int:subtraction>/"
+            "addition_specific_ans/<int:pages>/<int:ab_min>/<int:ab_max>/"
+            "<int:min_ans>/<int:max_ans>/<int:subtraction>/"
         ),
-        views.add_specific_ans,
-        name="add_specific_ans",
+        views.addition_specific_ans,
+        name="addition_specific_ans",
     ),
     path(
-        "diff_specific_ab/pages:<int:pages>/<int:ab_max>/<int:a>/<int:b>/",
-        views.diff_specific_ab,
-        name="diff_specific_ab",
+        (
+            "substraction_specific_ab/pages:<int:pages>/<int:ab_min>/"
+            "<int:ab_max>/<int:a>/<int:b>/"
+        ),
+        views.substraction_specific_ab,
+        name="substraction_specific_ab",
     ),
 ]
--- math_execrcise/sheets/views.py
+++ math_execrcise/sheets/views.py
@@ -8,41 +8,76 @@ from django.views import generic
 from .models import Question  # noqa: F401
 from .util import QuestionSheet
 
+
+class QuestionInterface:
+    def __init__(
+        self,
+        ans_min=0,
+        ans_max=10,
+        ab_min=0,
+        ab_max=10,
+        a=None,
+        b=None,
+        substruction=False,
+        num_of_questions=20,
+    ):
+
+        self.ans_min = ans_min
+        self.ans_max = ans_max
+        self.ab_min = ab_min
+        self.ab_max = ab_max
+        self.a = a
+        self.b = b
+        self.substruction = substruction
+        self.num_of_questions = num_of_questions
+
+    def generate(self):
+        pass
+
+
 class IndexView(generic.ListView):
     model = Question
     template_name = "sheets/index.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["created"] = timezone.now()
         return context
 
+
 def detail(request, action, pages):
     pass
 
 
-def add_specific_ab(request, pages, ab_max, a, b):
+def addition_specific_ab(request, pages, ab_min, ab_max, a, b):
     return generate_sheet(
-        question_add_specific_ab,
+        QuestionAdditionSpecificAb,
         pages=pages,
-        **dict(a=a, b=b, max_number=ab_max)
+        **dict(a=a, b=b, ab_min=ab_min, ab_max=ab_max)
     )
 
 
-def add_specific_ans(request, pages, ab_max, ans, width, subtraction):
+def addition_specific_ans(
+    request, pages, ab_min, ab_max, ans_min, ans_max, subtraction
+):
     return generate_sheet(
-        question_add_specific_ans,
+        QuestionAdditionSpecificAns,
         pages=pages,
         **dict(
-            ans=ans, width=width, max_number=ab_max, subtraction=subtraction
+            ans_min=ans_min,
+            ans_max=ans_max,
+            ab_min=ab_min,
+            ab_max=ab_max,
+            subtraction=subtraction,
         )
     )
 
 
-def diff_specific_ab(request, pages, ab_max, a, b):
+def substraction_specific_ab(request, pages, ab_min, ab_max, a, b):
     return generate_sheet(
-        question_diff_specific_ab,
+        QuestionSubstractionSpecificAb,
         pages=pages,
-        **dict(a=a, b=b, max_number=ab_max)
+        **dict(a=a, b=b, ab_min=ab_min, ab_max=ab_max)
     )
 
 
@@ -52,7 +87,7 @@ def generate_sheet(func, pages=10, **kwargs):
 
     sheets = QuestionSheet(response)
     for _ in range(pages):
-        questions, theme = func(**kwargs)
+        questions, theme = func(**kwargs).generate()
         sheets.draw(
             questions=questions, theme=theme,
         )
@@ -61,79 +96,86 @@ def generate_sheet(func, pages=10, **kwargs):
     return response
 
 
-def question_diff_specific_ab(max_number=10, a=None, b=None):
-    if not a and not b:
-        theme = "ひきざん（{}までのかず）".format(max_number)
-    elif a and b:
-        theme = "ひきざん（{}ひく{}）".format(a, b)
-    else:
-        if a:
-            theme = "ひきざん（{}ひく{}までのかず）".format(a, max_number)
+class QuestionSubstractionSpecificAb(QuestionInterface):
+    def generate(self):
+        if not self.a and not self.b:
+            theme = "ひきざん（{}までのかず）".format(self.ab_max)
+        elif self.a and self.b:
+            theme = "ひきざん（{}ひく{}）".format(self.a, self.b)
         else:
-            theme = "ひきざん（{}までのかずひく{}）".format(max_number, b)
+            if self.a:
+                theme = "ひきざん（{}ひく{}までのかず）".format(self.a, self.ab_max)
+            else:
+                theme = "ひきざん（{}までのかずひく{}）".format(self.ab_max, self.b)
 
-    questions = []
-    while len(questions) < 20:
-        if a:
-            question_a = a
+        questions = []
+        while len(questions) < self.num_of_questions:
+            if self.a:
+                question_a = self.a
+            else:
+                question_a = random.randint(0, self.ab_max)
+
+            if self.b:
+                question_b = -self.b
+            else:
+                question_b = -random.randint(0, question_a)
+            question = "{}{:+}=".format(question_a, question_b)
+            questions.append(question)
+        return questions, theme
+
+
+class QuestionAdditionSpecificAns(QuestionInterface):
+    def generate(self):
+        # todo: 入力エラー処理
+
+        if not self.subtraction:
+            theme = "たしざん（こたえが{}から{}になるけいさん）".format(
+                self.ans_min, self.ans_max
+            )
         else:
-            question_a = random.randint(0, max_number)
+            theme = "たしざんとひきざん（こたえが{}から{}になるけいさん）".format(
+                self.ans_min, self.ans_max
+            )
 
-        if b:
-            question_b = -b
+        questions = []
+        for _ in range(self.num_of_questions):
+            question_ans = random.randint(self.ans_min, self.ans_max)
+            if not self.subtraction:
+                question_a = random.randint(0, question_ans)
+                question_b = question_ans - question_a
+            else:
+                question_a = random.randint(question_ans, self.ab_max)
+                question_b = question_ans - question_a
+
+            question = "{}{:+}=".format(question_a, question_b)
+            questions.append(question)
+        return questions, theme
+
+
+class QuestionAdditionSpecificAb(QuestionInterface):
+    def generate(self):
+        if not self.a and not self.b:
+            theme = "たしざん（{}までのかず）".format(self.ab_max)
+        elif self.a and self.b:
+            theme = "たしざん（{}たす{}）".format(self.a, self.b)
         else:
-            question_b = -random.randint(0, question_a)
-        question = "{}{:+}=".format(question_a, question_b)
-        questions.append(question)
-    return questions, theme
+            if self.a:
+                theme = "たしざん（{}たす{}までのかず）".format(self.a, self.ab_max)
+            else:
+                theme = "たしざん（{}までのかずたす{}）".format(self.ab_max, self.b)
 
+        questions = []
+        for _ in range(self.num_of_questions):
+            if self.a:
+                question_a = self.a
+            else:
+                question_a = random.randint(0, self.ab_max)
 
-def question_add_specific_ans(ans=10, width=0, max_number=20, subtraction=0):
-    # todo: 入力エラー処理
+            if self.b:
+                question_b = self.b
+            else:
+                question_b = random.randint(0, self.ab_max)
 
-    if not subtraction:
-        theme = "たしざん（だいたい{}になるけいさん、はば{}）".format(ans, width)
-    else:
-        theme = "たしざんとひきざん（だいたい{}になるけいさん、はば{}）".format(ans, width)
-
-    questions = []
-    for _ in range(20):
-        question_ans = random.randint(ans - width, ans + width)
-        if not subtraction:
-            question_a = random.randint(0, question_ans)
-            question_b = question_ans - question_a
-        else:
-            question_a = random.randint(question_ans, max_number)
-            question_b = question_ans - question_a
-
-        question = "{}{:+}=".format(question_a, question_b)
-        questions.append(question)
-    return questions, theme
-
-
-def question_add_specific_ab(a=None, b=None, max_number=10):
-    if not a and not b:
-        theme = "たしざん（{}までのかず）".format(max_number)
-    elif a and b:
-        theme = "たしざん（{}たす{}）".format(a, b)
-    else:
-        if a:
-            theme = "たしざん（{}たす{}までのかず）".format(a, max_number)
-        else:
-            theme = "たしざん（{}までのかずたす{}）".format(max_number, b)
-
-    questions = []
-    for _ in range(20):
-        if a:
-            question_a = a
-        else:
-            question_a = random.randint(0, max_number)
-
-        if b:
-            question_b = b
-        else:
-            question_b = random.randint(0, max_number)
-
-        question = "{}+{}=".format(question_a, question_b)
-        questions.append(question)
-    return questions, theme
+            question = "{}+{}=".format(question_a, question_b)
+            questions.append(question)
+        return questions, theme
