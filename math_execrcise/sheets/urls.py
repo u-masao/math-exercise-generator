6375118f22f11339f8c94b2e3004c688cc01dbad
u-masao <4973920+u-masao@users.noreply.github.com>
2020-08-22 23:21:41 +0900

    URLをシンプルにした

--- math_execrcise/sheets/urls.py
+++ math_execrcise/sheets/urls.py
@@ -3,11 +3,16 @@ from django.urls import path
 from . import views
 
 app_name = "sheets"
-_url_params = (
-    "/<int:pages>/<int:ab_min>/<int:ab_max>/"
-    "<int:ans_min>/<int:ans_max>/<int:a>/<int:b>/"
-)
+_url_params = ()
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("action/<str:action>" + _url_params, views.action, name="action",),
+    path(
+        (
+            "pdf/<str:action>/"
+            "<int:pages>/<int:ab_min>/<int:ab_max>/"
+            "<int:ans_min>/<int:ans_max>/<int:a>/<int:b>/"
+        ),
+        views.pdf,
+        name="pdf",
+    ),
 ]
