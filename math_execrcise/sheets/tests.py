b31b76527bf22bdea727140dcca5c54d0b692ac2
u-masao <4973920+u-masao@users.noreply.github.com>
2020-08-24 21:35:20 +0900

    add test

--- math_execrcise/sheets/tests.py
+++ math_execrcise/sheets/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase  # noqa: F401
+from django.test import TestCase
+from sheets.views import (
+    QuestionAdditionSpecificAb,
+    QuestionSubtractionSpecificAb,
+    QuestionSpecificAns,
+)
 
-# Create your tests here.
+
+class QuestionAdditionSpecificAbTests(TestCase):
+    def test_default_constructor(self):
+        num_of_questions = 40
+        question_generators = [
+            QuestionAdditionSpecificAb,
+            QuestionSubtractionSpecificAb,
+            QuestionSpecificAns,
+        ]
+        for question_generator in question_generators:
+            questions = question_generator(num_of_questions=num_of_questions)
+            question_list, theme = questions.generate()
+            self.assertTrue(isinstance(theme, str))
+            self.assertTrue(isinstance(question_list, list))
+            for question_str in question_list:
+                self.assertTrue(isinstance(question_str, str))
+                self.assertTrue("=" in question_str)
+
+            self.assertEqual(len(question_list), num_of_questions)
