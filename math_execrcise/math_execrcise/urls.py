c06b693470b507021257cba6def6d063b2edb98b
u-masao <4973920+u-masao@users.noreply.github.com>
2020-10-31 09:17:07 +0900

    remove polls

--- math_execrcise/math_execrcise/urls.py
+++ math_execrcise/math_execrcise/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("sheets/", include("sheets.urls")),
-    path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
--- math_execrcise/polls/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig  # noqa: F401
-
-
-class PollsConfig(AppConfig):
-    name = "polls"
