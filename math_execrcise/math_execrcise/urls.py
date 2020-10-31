12d4b9f6887b9fb4dcc71f2f548eb1f7d57aecb3
u-masao <4973920+u-masao@users.noreply.github.com>
2020-10-31 10:36:24 +0900

    change url routing

--- math_execrcise/math_execrcise/urls.py
+++ math_execrcise/math_execrcise/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("sheets.urls")),
-    path("sheets/", include("sheets.urls")),
+    # path("sheets/", include("sheets.urls")),
     path("admin/", admin.site.urls),
 ]
