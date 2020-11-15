337ea4f6dfaf0562ec6a4715c61b118efe41cf4a
u-masao <4973920+u-masao@users.noreply.github.com>
2020-11-15 10:30:59 +0900

    タイトル変更。レイアウト変更に向けてコードを整理。

--- math_execrcise/sheets/question_sheet.py
+++ math_execrcise/sheets/question_sheet.py
@@ -6,60 +6,73 @@ from reportlab.lib.units import cm
 from reportlab.pdfbase.ttfonts import TTFont
 
 GEN_SHIN_GOTHIC_MEDIUM_TTF = "./sheets/fonts/GenShinGothic-P-Medium.ttf"
+GEN_SHIN_GOTHIC_FONT_NAME = "GenShinGothic"
 
 
 class QuestionSheet:
     def __init__(
-        self, output, author="author", title="title", subject="subject"
+        self,
+        output,
+        author="author",
+        title="title",
+        subject="subject",
+        page_width_cm=21.0,
+        page_height_cm=29.7,
     ):
         self.pdf_canvas = canvas.Canvas(output)
         self.pdf_canvas.setAuthor(author)
         self.pdf_canvas.setTitle(title)
         self.pdf_canvas.setSubject(subject)
-        self.pdf_canvas.setPageSize((21.0 * cm, 29.7 * cm))
+        self.pdf_canvas.setPageSize((page_width_cm * cm, page_height_cm * cm))
 
-    def set_font(self, font_name, font_size):
-        pdfmetrics.registerFont(TTFont(font_name, GEN_SHIN_GOTHIC_MEDIUM_TTF))
-        self.pdf_canvas.setFont(font_name, font_size)
+    def set_fontsize(self, font_size):
+        pdfmetrics.registerFont(
+            TTFont(GEN_SHIN_GOTHIC_FONT_NAME, GEN_SHIN_GOTHIC_MEDIUM_TTF)
+        )
+        self.pdf_canvas.setFont(GEN_SHIN_GOTHIC_FONT_NAME, font_size)
 
     def close(self):
         self.pdf_canvas.save()
 
-    def build_cells(self, questions):
+    def _build_cells(self, questions, rows=10, cols=2):
         cells = []
         index = 0
-        for a in range(1, 11):
+        for a in range(1, rows + 1):
             row = []
-            for b in range(1, 3):
+            for b in range(1, cols + 1):
                 cell = questions[index]
                 index = (1 + index) % len(questions)
                 row.append(cell)
             cells.append(row)
         return cells
 
+    def _draw_string(
+        self, text, x, y, size,
+    ):
+        self.set_fontsize(size)
+        self.pdf_canvas.drawString(x * cm, y * cm, text)
+
     def draw(
         self,
         questions=["0+0="],
+        rows=10,
+        cols=2,
         theme="なし",
-        page_title="ぱぱのさんすうもんだい",
-        font_name="GenShinGothic",
+        page_title="さんすうもんだいをやろう！",
+        page_subtitle="「=」のみぎがわに、ただしいすうじをかいてね",
     ):
 
+        cells = self._build_cells(questions, rows=rows, cols=cols)
+
         self.pdf_canvas.setTitle(page_title)
-
-        cells = self.build_cells(questions)
-
-        self.set_font(font_name, 30)
-        self.pdf_canvas.drawString(2 * cm, 26 * cm, page_title)
-
-        self.set_font(font_name, 20)
-        self.pdf_canvas.drawString(3 * cm, 25 * cm, "「=」のみぎがわに、ただしいすうじをかいてね")
-
-        self.set_font(font_name, 12)
-        self.pdf_canvas.drawString(5 * cm, 1 * cm, "テーマ「{}」".format(theme))
+        self._draw_string(page_title, x=2, y=26, size=30)
+        self._draw_string(page_subtitle, x=3, y=25, size=20)
+        self._draw_string(f"テーマ「{theme}」", x=3, y=2, size=12)
 
         table = Table(cells, colWidths=8 * cm, rowHeights=2 * cm)
-        table.setStyle([("FONT", (0, 0), (-1, -1), font_name, 28)])
+        table.setStyle(
+            [("FONT", (0, 0), (-1, -1), GEN_SHIN_GOTHIC_FONT_NAME, 28)]
+        )
         w, h = table.wrapOn(self.pdf_canvas, 0, 0)
         table.drawOn(self.pdf_canvas, 3 * cm, 24 * cm - h)
 
