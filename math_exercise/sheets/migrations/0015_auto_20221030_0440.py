384d4502542e42289507c82e44940ca7e66b803e
u-masao <4973920+u-masao@users.noreply.github.com>
2022-12-26 12:40:13 +0900

    [update] bugfix. fixture file name

--- math_exercise/sheets/migrations/0015_auto_20221030_0440.py
+++ math_exercise/sheets/migrations/0015_auto_20221030_0440.py
@@ -5,8 +5,8 @@ from django.db import migrations
 
 
 def load_fixture(apps, schema_editor):
-    call_command("loaddata", "sheets/fixture/0014/algorythms.json", app_label="sheets")
-    call_command("loaddata", "sheets/fixture/0014/questions.json", app_label="sheets")
+    call_command("loaddata", "sheets/fixture/0015/algorythms.json", app_label="sheets")
+    call_command("loaddata", "sheets/fixture/0015/questions.json", app_label="sheets")
 
 
 class Migration(migrations.Migration):
